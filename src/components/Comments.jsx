768d79eb26e2e3b8a8b50e12b00679d9f8c98c0c
Maxwell Kendall <maxwellkendall@C02NVEGHG3QC.local>
2018-03-31 19:09:26 -0400

    working to get this done now

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -15,6 +15,7 @@ export default class Comments extends Component {
     msg: '',
     loading: false,
     success: false,
+    index: 10,
   }
 
   componentDidMount() {
@@ -22,7 +23,7 @@ export default class Comments extends Component {
   }
 
   getComments = () => {
-    this.props.getComments(10);
+    this.props.getComments(this.state.index);
     // this.setState(prevState => ({ ...prevState, hideComments: !prevState.hideComments }));
   }
 
@@ -45,8 +46,17 @@ export default class Comments extends Component {
     axios.post('/api/comments', { userid: this.props.activeUser.id, memory: this.state.msg })
       .then(() => {
         this.setState(prevState => ({ ...prevState, success: true, msg: '' }));
+        this.getComments(this.state.index);
       })
       .catch(err => console.log('error'));
+  }
+
+  seeMore = () => {
+    this.setState(prevState => ({ ...prevState, index: prevState.index + 5 }), this.getComments);
+  }
+
+  seeLess = () => {
+    this.setState(prevState => ({ ...prevState, index: prevState.index - 5 }), this.getComments);
   }
 
   render() {
@@ -57,14 +67,19 @@ export default class Comments extends Component {
         <ul className={cx('comments')}>
           {this.displayComments(comments)}
         </ul>
-        <Form onSubmit={this.handleSubmit} success={this.state.success} >
-          <Form.Field>
+        <span className="comments__toggleView">
+          <a className="comments__toggleView--more" onClick={this.seeMore}>See More</a>
+          <a className="comments__toggleView--less" onClick={this.seeLess}>See Less</a>
+        </span>
+        <Form className={'comments__form'} onSubmit={this.handleSubmit} success={this.state.success} >
+          <Form.Field className="comments__explaination">
             <h2>What should I say?</h2>
             <p>The idea here is to remind Ross of a good experience you've had with him at work. It could be something as simple as a regular lunch. Don't be shy!</p>
           </Form.Field>
-          <Form.Field>
+          <Form.Field className="comments__field">
             <label htmlFor="memory">Enter a Memory</label>
             <textarea
+              className="comments__textarea"
               maxLength="1000"
               name="memory"
               cols="30"
@@ -74,7 +89,7 @@ export default class Comments extends Component {
               placeholder={`Ross! It's your ole pal ${activeUser.name}! Remember when... Hope you have a great day!`}
             />
           </Form.Field>
-          <Button type='submit' disabled={this.state.success}>Submit</Button>
+          <Button onSubmit={this.handleSubmit} type='submit'>Submit</Button>
         </Form>
       </div>
     );
